902f724f302c0559334ab5be08f872276e424c7e
hosokawa-kenshin <hosokawa2023@s.okayama-u.ac.jp>
2024-10-22 17:51:22 +0900

    Implement Notification For All using topic

--- lib/main.dart
+++ lib/main.dart
@@ -43,12 +43,15 @@ void main() async {
     await androidImplementation?.requestNotificationsPermission();
   }
 
-  // 通知設定の初期化を行う
   _initNotification();
 
-  // アプリ停止時に通知をタップした場合はgetInitialMessageでメッセージデータを取得できる
   final message = await FirebaseMessaging.instance.getInitialMessage();
   // 取得したmessageを利用した処理などを記載する
+
+  final messageForAllProduct = await FirebaseMessaging.instance.subscribeToTopic("Product");
+  final messageForAllShop = await FirebaseMessaging.instance.subscribeToTopic("Shop");
+  final messageForAllAnnounce = await FirebaseMessaging.instance.subscribeToTopic("Announce");
+
   runApp(
     const MyApp(),
   );
